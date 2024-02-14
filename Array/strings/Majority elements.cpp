86e3f7401b6fc28b6d7a174bbfc957179a36545d
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2024-02-14 23:58:15 +0530

    .

--- Array/strings/Majority elements.cpp
+++ Array/strings/Majority elements.cpp
@@ -3,13 +3,13 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> hash;
         int result = 0;
-        int majority = 0;
+        int majoritys = 0;
 
         for (int n : nums) {
             hash[n] = 1 + hash[n];
-            if (hash[n] > majority) {
-                res = n;
-                majority = hash[n];
+            if (hash[n] > majoritys) {
+                result = n;
+                majoritys = hash[n];
             }
         }
 
