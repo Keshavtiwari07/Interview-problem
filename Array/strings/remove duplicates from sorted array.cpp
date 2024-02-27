3d7d4d41f8724e7a6dba2e003f26f8fa2a8500c9
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2024-02-27 23:59:56 +0530

    .

--- Array/strings/remove duplicates from sorted array.cpp
+++ Array/strings/remove duplicates from sorted array.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 1; i < nums.size(); i++){
             if(nums[i] != nums[i - 1]){
                 nums[j] = nums[i];
-                j++;
+                j++;//.
             }
         }
         return j;
